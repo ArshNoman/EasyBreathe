0e57d77a32656a3a0059f86d9edd36f1a21e871a
arsh <arshnoman2270@gnail.com>
2024-11-02 17:04:51 -0400

    Added the Web app foundations

---
+++ app.py
@@ -0,0 +1,17 @@
+""" Here is this project's framework for the webapp. Made with Flask. """
+
+from flask import Flask, render_template, request, redirect, flash
+import datetime
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'theory can only take you so far'
+
+
+@app.route("/", methods=['POST', 'GET'])
+def main_page():
+
+    return render_template('homepage.html')
+
+
+if __name__ == '__main__':
+    Flask.run(app)
