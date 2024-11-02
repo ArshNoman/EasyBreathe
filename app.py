43f4a3e33d21abd2b39f8c7b415713597f7071e5
arsh <arshnoman2270@gnail.com>
2024-11-02 17:27:01 -0400

    database added

--- app.py
+++ app.py
@@ -4,10 +4,28 @@ from flask import Flask, render_template, request, redirect, flash
 import datetime
 import pymysql
 
+# Creating the Flask app for development and deployment
 app = Flask(__name__)
+# Great movie!
 app.config['SECRET_KEY'] = 'theory can only take you so far'
 
+# Creating the first connection to our database
+db = pymysql.connect(
+    host="byfjbhyqlcuhrwqfxi7f-mysql.services.clever-cloud.com",
+    user="ulrhleoemwrfziuv",
+    password="fGmH4eiPDxk3paoPxy3i",
+    database="byfjbhyqlcuhrwqfxi7f",
+    autocommit=True)
 
+# Initiating the database cursor for making changes
+cursor = db.cursor()
+
+# Connecting to the MySQL database
+db.ping()
+if cursor.connection is None:
+    db.ping()
+
+# Main page function
 @app.route("/", methods=['POST', 'GET'])
 def main_page():
 
