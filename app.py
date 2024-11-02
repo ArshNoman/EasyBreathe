57ae3a180495ee470df6106ddeef96750283a1ae
arsh <arshnoman2270@gnail.com>
2024-11-02 17:24:45 -0400

    Added database infrastructure foundations

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-""" Here is this project's framework for the webapp. Made with Flask. """
+""" This is the project's framework for the webapp. Made with Flask. """
 
 from flask import Flask, render_template, request, redirect, flash
 import datetime
+import pymysql
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'theory can only take you so far'
