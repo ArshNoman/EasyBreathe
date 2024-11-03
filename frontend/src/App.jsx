102f0e27c3d716f7b305b06f5a6071aeb066222a
Kriti-Negi <66047981+Kriti-Negi@users.noreply.github.com>
2024-11-02 20:40:42 -0400

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,10 +7,25 @@ import './styles/sidebar.css'
 import PhoneForm from './components/PhoneForm'
 import MapComponent from './components/MapComponent';
 
-
-
-
 const App = () => {
+
+  const [date, setDate] = useState("0-0-0");
+  const [time, setTime] = useState("");
+
+  function parseDateAndTime(d){
+    d = "" + d;
+    let day = d.substring(8, 10);
+    let y = d.substring(11, 15);
+    let mString = d.substring(4, 7);
+    let m = "";
+    if(mString == "Nov"){
+      m = "11"
+    }
+    setDate(y + "-" + m + "-" + day);
+    console.log(d.substring(16, 18) +":00");
+    setTime(d.substring(16, 18) +":00");
+  }
+
   return (
     <div className='container'>
         <div className='sidebar'>
@@ -21,10 +36,14 @@ const App = () => {
           <div className='form'>
             <div className='form-group'>
               <label>Date: </label>
-              <input type="date" id="selected_date" name="date"/>
+              <input type="date" id="selected_date" value = {date} name="date" onChange={(e) => setDate(e.target.value)}/>
             </div>
             <div className='form-group'>
-              <button>Today</button>
+              <label>Time: </label>
+              <input type="time" id="selected_time" value = {time} name="date" onChange={(e) => setTime(e.target.value)}/>
+            </div>
+            <div className='form-group'>
+              <button onClick={() => {parseDateAndTime(new Date)}}>Now</button>
             </div>
           </div>
           <PhoneForm/>
@@ -38,7 +57,3 @@ const App = () => {
 };
 
 export default App;
-
-/*
-
-      */
